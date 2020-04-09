b128b6b699f6552b710d906447f0508afd4ddae4
JACINTO WONG <jacinto.wong@cbc.ca>
2020-04-09 11:53:25 -0400

    lint and prettier

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-const videoElement = document.getElementById('videoElement');
-const pipButtonElement = document.getElementById('pipButtonElement');
+const videoElement = document.getElementById("videoElement");
+const pipButtonElement = document.getElementById("pipButtonElement");
 
-pipButtonElement.addEventListener('click', async function() {
-    pipButtonElement.disabled = true;
+pipButtonElement.addEventListener("click", async function() {
+  pipButtonElement.disabled = true;
 
-    await videoElement.requestPictureInPicture();
+  await videoElement.requestPictureInPicture();
 
-    pipButtonElement.disabled = false;
+  pipButtonElement.disabled = false;
 });
 
-navigator.mediaDevices.getDisplayMedia()
-    .then(function (mediaStream) {
-        videoElement.srcObject = mediaStream;
-        videoElement.onloadedmetadata = function (e) {
-            videoElement.play();
-        };
-    })
+navigator.mediaDevices.getDisplayMedia().then(function(mediaStream) {
+  videoElement.srcObject = mediaStream;
+  videoElement.onloadedmetadata = function (e) {
+    videoElement.play();
+  };
+});
