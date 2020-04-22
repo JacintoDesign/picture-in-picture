f2ec5691db0635b7fad77a4587c455801bbf27c9
JACINTO WONG <jacinto.wong@cbc.ca>
2020-04-21 21:34:45 -0400

    final production code

--- script.js
+++ script.js
@@ -4,16 +4,24 @@ const button = document.getElementById('pipButton');
 button.addEventListener('click', async () => {
   // Disable Button
   button.disabled = true;
-  // Button Starts Picture in Picture
+  // Start Picture in Picture
   await videoElement.requestPictureInPicture();
   // Reset Button
   button.disabled = false;
 });
 
-// Prompt To Select Media Stream On Load, Playing Streaming Video
-navigator.mediaDevices.getDisplayMedia().then((mediaStream) => {
-  videoElement.srcObject = mediaStream;
-  videoElement.onloadedmetadata = () => {
-    videoElement.play();
-  };
-});
+// Prompt to select media stream, pass to video element, then play
+async function selectMediaStream() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
+  } catch (error) {
+    // Catch Error Here
+  }
+}
+
+// On Load
+selectMediaStream();
