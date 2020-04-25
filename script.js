2f200f3c2c4a938e5f09f5870ff0bc4ae3c6e7ac
JACINTO WONG <jacinto.wong@cbc.ca>
2020-04-25 10:45:44 -0400

    final production code

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
 const videoElement = document.getElementById('video');
 const button = document.getElementById('button');
 
-button.addEventListener('click', async () => {
-  // Disable Button
-  button.disabled = true;
-  // Start Picture in Picture
-  await videoElement.requestPictureInPicture();
-  // Reset Button
-  button.disabled = false;
-});
-
 // Prompt to select media stream, pass to video element, then play
 async function selectMediaStream() {
   try {
@@ -22,6 +13,15 @@ async function selectMediaStream() {
     // Catch Error Here
   }
 }
+
+button.addEventListener('click', async () => {
+  // Disable Button
+  button.disabled = true;
+  // Start Picture in Picture
+  await videoElement.requestPictureInPicture();
+  // Reset Button
+  button.disabled = false;
+});
 
 // On Load
 selectMediaStream();
