2a9e2e8b165b4bd905ccbd8c0c94a2c9ef9caf52
Jinhao Jiang <58126838+DavinJ-0316@users.noreply.github.com>
2021-01-09 15:39:05 +1100

    Add files via upload

--- script.js
+++ script.js
@@ -3,24 +3,27 @@ const button = document.getElementById('button');
 
 // Prompt to select media stream, pass to video element, then play
 async function selectMediaStream() {
-  try {
-    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
-    videoElement.srcObject = mediaStream;
-    videoElement.onloadedmetadata = () => {
-      videoElement.play();
-    };
-  } catch (error) {
-    // Catch Error Here
-  }
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+
+        videoElement.srcObject = mediaStream;
+
+        videoElement.onloadedmetadata = ()=> {
+            videoElement.play();
+        }
+    } catch (error) {
+        // Catch Error Here
+        console.log('whoops, error here:', error);
+    }
 }
 
 button.addEventListener('click', async () => {
-  // Disable Button
-  button.disabled = true;
-  // Start Picture in Picture
-  await videoElement.requestPictureInPicture();
-  // Reset Button
-  button.disabled = false;
+    //Disable Button
+    button.disabled = true;
+    //Start Picture in Picture
+    await videoElement.requestPictureInPicture();
+    //Reset Button
+    button.disabled = false;
 });
 
 // On Load
